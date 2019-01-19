e7ad4b61e12ee9ce022d1e663b37d7905fb61f61
Vagner Santos <santos.vagnerferreira@gmail.com>
2019-01-19 12:31:59 -0200

    Adds logic to render minimal components

--- app/components/AddEntry.js
+++ app/components/AddEntry.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { View } from "react-native";
 import { getMetricMetaInfo } from "../utils/helpers";
+import UdaciSlider from "./UdaciSlider";
+import UdaciSteppers from "./UdaciSteppers";
+import DateHeader from "./DateHeader";
 
 export default class AddEntry extends Component {
   state = {
@@ -42,6 +45,37 @@ export default class AddEntry extends Component {
   };
 
   render() {
-    return <View>{getMetricMetaInfo("bike").getIcon()}</View>;
+    const metaInfo = getMetricMetaInfo();
+
+    return (
+      <View>
+        <DateHeader date={new Date().toLocaleDateString()} />
+
+        {Object.keys(metaInfo).map(key => {
+          const { getIcon, type, ...rest } = metaInfo[key];
+          const value = this.state[key];
+
+          return (
+            <View key={key}>
+              {getIcon()}
+              {type === "slider" ? (
+                <UdaciSlider
+                  value={value}
+                  onChange={value => this.slide(key, value)}
+                  {...rest}
+                />
+              ) : (
+                <UdaciSteppers
+                  value={value}
+                  onIncrement={() => this.increment(key)}
+                  onDecrement={() => this.decrement(key)}
+                  {...rest}
+                />
+              )}
+            </View>
+          );
+        })}
+      </View>
+    );
   }
 }
