13b3912feefa0bd26370d0706eefec09f9737d5b
Vagner Santos <santos.vagnerferreira@gmail.com>
2019-01-20 21:51:38 -0200

    Adds Redux

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { View } from "react-native";
 import AddEntry from "./app/components/AddEntry";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./app/reducers";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <AddEntry />
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>
+          <AddEntry />
+        </View>
+      </Provider>
     );
   }
 }
